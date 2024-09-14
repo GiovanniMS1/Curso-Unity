f797de6e0947a3162aa864ee81367af727f618a6
GiovanniMS1 <giovanni.mar.san2003@gmail.com>
2024-09-14 17:02:12 -0300

    Commit 2
    NavMesh + Player + Organização

---
+++ Assets/_SRC/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Layers")]
+    public LayerMask groundLayer;
+
+    private NavMeshAgent nav;
+
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit,Mathf.Infinity, groundLayer))
+        {
+            if(hit.collider.CompareTag("Ground"))
+            {
+                Debug.Log("HITANDO CHAO");
+
+                nav.SetDestination(hit.point);
+            }
+        }
+    }
+}
